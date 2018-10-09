53ccc19c3220611094dd322c54719ee590efce87
Lui de la Parra <lui@mutesymphony.com>
2018-10-09 11:18:51 -0700

    Adds Project  Dependencies
    This commit adds the filemaker-api-client, nodestream, fs-extram and axios-cookiejar support.
    
    Signed-off-by: Lui de la Parra <lui@mutesymphony.com>

--- services/index.js
+++ services/index.js
@@ -2,17 +2,21 @@
 
 const { Account } = require('./account');
 const { authentication } = require('./authentication');
+const { image } = require('./image');
 const { logger } = require('./logger');
 const { token } = require('./token');
 const { upload } = require('./upload');
 const { validation, Joi } = require('./validation');
+const { Filemaker } = require('fms-api-client')
 
 module.exports = {
   Account,
   authentication,
+  image,
   logger,
   token,
   upload,
   validation,
-  Joi
+  Joi,
+  Filemaker
 };
