117c2cb7ed1a1f18d5018b1ccf6c96815b862d12
Lui de la Parra <lui@mutesymphony.com>
2018-10-20 14:47:30 -0700

    Adds Sudo To CodeDeploy Install
    This command adds Sudo to the Code Deploy Install
    
    Signed-off-by: Lui de la Parra <lui@mutesymphony.com>

--- deploy/start.sh
+++ deploy/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source /home/ec2-user/.bash_profile
 cd /home/ec2-user/big-brother
-npm install
-npm install face-recognition
+sudo npm install
+sudo npm install face-recognition
 cd /home/ec2-user
 pm2 start ecosystem.config.js
