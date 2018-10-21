02ab1a35cb38c9b1e6dece9b5519036e797fb9f3
Lui de la Parra <lui@mutesymphony.com>
2018-10-20 17:49:56 -0700

    Revises CodeDeploy Configuration
    This commit removes CodeDeploy configuration that was added to triage deploy issue
    
    Signed-off-by: Lui de la Parra <lui@mutesymphony.com>

--- deploy/start.sh
+++ deploy/start.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 source /home/ec2-user/.bash_profile
 cd /home/ec2-user/big-brother
-sudo npm install
-sudo npm install face-recognition
+npm install
 cd /home/ec2-user
 pm2 start ecosystem.config.js
